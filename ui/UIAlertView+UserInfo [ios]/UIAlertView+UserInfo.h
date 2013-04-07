f5bef93df8e32c9e21ba35c03bb68adf4dde86b3
Dominik Pich <Dominik@Pich.info>
2013-04-07 22:22:09 +0200

    added UIAlertView category that provides a userInfo property

---
+++ ui/UIAlertView+UserInfo [ios]/UIAlertView+UserInfo.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView (UserInfo)
+//  DecisionMaker
+//
+//  Created by Dominik Pich on 01.04.13.
+//  Copyright (c) 2013 Dominik Pich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (UserInfo)
+@property (nonatomic, assign) NSDictionary* userInfo;
+@end
