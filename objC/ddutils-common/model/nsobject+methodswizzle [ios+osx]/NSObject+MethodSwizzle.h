5d94264e47d864251292ecff9ad5b136d99fc161
Dominik Pich <dpich@sapient.com>
2015-09-20 13:57:28 +0200

    updated swille method
    Signed-off-by: Dominik Pich <dpich@sapient.com>

--- objC/ddutils-common/model/nsobject+methodswizzle [ios+osx]/NSObject+MethodSwizzle.h
+++ objC/ddutils-common/model/nsobject+methodswizzle [ios+osx]/NSObject+MethodSwizzle.h
@@ -1,22 +1,19 @@
-//
-//  NSObject+MethodSwizzle.h
-//
-
+/**
+@file      NSObject+MethodSwizzle.h
+@author    Sapient GmbH
+@date      2013-05-31
+@copyright AUDI AG, 2014. All Rights Reserved
+*/
 #import <Foundation/Foundation.h>
 
 @interface NSObject (MethodSwizzle)
 
 + (void)swizzleInstanceMethodWithSelector:(SEL)original_selector withSelector:(SEL)alternative_selector;
 
-+ (void *)swizzleInstanceMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f;
++ (void*)swizzleInstanceMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f;
 
 //---
 
 + (void)swizzleClassMethodWithSelector:(SEL)original_selector withSelector:(SEL)alternative_selector;
 
-+ (void *)swizzleClassMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f;
-
 @end
-
-///THE PARAMETER withFunction:(IMP)f can take a block or a c-function
-///e.g.     IMP myIMP = imp_implementationWithBlock(^(id _self, NSString *string) {...});
--- objC/ddutils-common/model/nsobject+methodswizzle [ios+osx]/NSObject+MethodSwizzle.m
+++ objC/ddutils-common/model/nsobject+methodswizzle [ios+osx]/NSObject+MethodSwizzle.m
@@ -1,6 +1,9 @@
-//
-//  NSObject+MethodSwizzle.m
-//
+/**
+@file      NSObject+MethodSwizzle.m
+@author    Sapient GmbH
+@date      2013-05-01
+@copyright AUDI AG, 2014. All Rights Reserved
+*/
 #import "NSObject+MethodSwizzle.h"
 #import <objc/runtime.h>
 
@@ -16,43 +19,19 @@
     }
 }
 
-+ (void *)swizzleInstanceMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f {
++ (void*)swizzleInstanceMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f {
     Method origMethod = class_getInstanceMethod(trgt, originalSelector);
-    void *origImp = (void  *)method_getImplementation(origMethod);
+    void *origImp = (void *)method_getImplementation(origMethod);
     
-    if (!class_addMethod(trgt, originalSelector, f, method_getTypeEncoding(origMethod)))
+    if(!class_addMethod(trgt, originalSelector, f, method_getTypeEncoding(origMethod)))
         method_setImplementation(origMethod, f);
     
     return origImp;
 }
 
-//---
-
-+ (void)swizzleClassMethodWithSelector:(SEL)original_selector withSelector:(SEL)override_selector {
-    const char *name = class_getName(self.class);
-    Class meta = objc_getMetaClass(name);
-    
-    Method originalMethod = class_getClassMethod(self, original_selector);
-    Method overrideMethod = class_getClassMethod(self, override_selector);
-    if (class_addMethod(meta, original_selector, method_getImplementation(overrideMethod), method_getTypeEncoding(overrideMethod))) {
-        class_replaceMethod(meta, override_selector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-    } else {
-        method_exchangeImplementations(originalMethod, overrideMethod);
-    }
++ (void)swizzleClassMethodWithSelector:(SEL)original_selector withSelector:(SEL)alternative_selector; {
+    Method origMethod = class_getClassMethod(self, original_selector);
+    Method newMethod = class_getClassMethod(self, alternative_selector);
+    method_exchangeImplementations(origMethod, newMethod);
 }
-
-+ (void *)swizzleClassMethodWithSelector:(SEL)originalSelector ofClass:(Class)trgt withFunction:(IMP)f {
-    const char *name = class_getName(trgt);
-    Class meta = objc_getMetaClass(name);
-
-    Method origMethod = class_getClassMethod(trgt, originalSelector);
-    void *origImp = (void  *)method_getImplementation(origMethod);
-    
-    if (!class_addMethod(meta, originalSelector, f, method_getTypeEncoding(origMethod)))
-        method_setImplementation(origMethod, f);
-    
-    return origImp;
-}
-
-
 @end
