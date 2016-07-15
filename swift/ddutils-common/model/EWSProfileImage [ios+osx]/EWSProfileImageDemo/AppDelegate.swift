123387e15f3c98b9d09eabd1428e380a8f0499c0
Dominik Pich <Dominik@pich.info>
2016-07-15 16:54:42 +0200

    EWSProfileImages class
    Signed-off-by: Dominik Pich <Dominik@pich.info>

--- swift/ddutils-common/model/EWSProfileImage [ios+osx]/EWSProfileImageDemo/AppDelegate.swift
+++ swift/ddutils-common/model/EWSProfileImage [ios+osx]/EWSProfileImageDemo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         //setup ews
-        EWSProfileImages.shared.credentials = (host:"webmail.sapient.com", username: "dpich", password: "***REMOVED***")
+        EWSProfileImages.shared.credentials = (host:"webmail.sapient.com", username: "TODO", password: "TODO")
 
         return true
     }
